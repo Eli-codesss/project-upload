e45b444d0a824be7c1c9e3a87fe5a95615ec845b
Pius Prince <elikpliyaa@gmail.com>
2024-08-16 10:58:27 +0000

    chore: Remove sign in page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,11 +7,18 @@ import {
 } from "@clerk/nextjs";
 import { Button } from "./ui/button";
 import Link from "next/link";
+import UmatLogo from "../../public/umat-removebg-preview.webp";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <nav className="container mx-auto flex flex-wrap items-center justify-between p-6">
-      <Link href="/">UMaT-SRID Project Repository </Link>
+      <Link href="/" className="text-xl font-bold">
+        <span className="flex items-center">
+          <Image src={UmatLogo} alt="UMaT Logo" width={32} height={32} />
+          UMaT-SRID Project Repository{" "}
+        </span>
+      </Link>
       <div>
         <OrganizationSwitcher />
         <UserButton />
